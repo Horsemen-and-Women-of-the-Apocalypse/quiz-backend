dfa9a9e26a55c0db305f4c1a7ea5d177a9ca613a
tom.mansion <tomansion@yahoo.fr>
2021-05-19 22:11:56 +0200

    WIP credentials test

--- src/services/ws.js
+++ src/services/ws.js
@@ -4,6 +4,8 @@ import AuthError from "../common/autherror";
 import LOGGER from "../utils/logger";
 
 class WebsocketService {
+
+
     /**
      * Initialize websocket
      *
@@ -14,10 +16,11 @@ class WebsocketService {
         LOGGER.info("[WS] Websocket opened on localhost" + this.ws._path);
 
         // Define middleware
-        this.ws.use(this.middleware);
+        this.ws.use((socket, next) => { this.middleware(socket, next) });
 
         // Define connection callback
         this.ws.on("connect", this.onConnection);
+        this.ws.on("connection", this.onConnection);
     }
 
     /**
@@ -28,9 +31,20 @@ class WebsocketService {
      * @return {Promise<void>} Promise
      */
     async middleware(socket, next) {
-        // Reject packet
-        next(new AuthError(AUTH.ACCESS_DENIED));
+        try {
+            console.log('middelware');
+            let { player, lobby } = await this.checkUserAccess(socket.handshake.query);
+            console.log(player);
+            console.log(lobby);
+            next();
+
+
+        } catch (error) {
+            LOGGER.warn("[WS] Failed websocket connection : " + error.message);
+            next(error);
+        }
     }
+
 
     /**
      * Callback on client connection
@@ -38,12 +52,17 @@ class WebsocketService {
      * @param client Client
      */
     onConnection(client) {
+        console.log('onConnection');
+
         LOGGER.info("[WS] Connection opened with " + client.handshake.address);
 
         client.emit("connection", "CONNECTION_OPENED");
 
         client.on("disconnect", () => {
             LOGGER.info("[WS] Connection closed with " + client.handshake.address);
+        });
+        client.on("joinLobby", (p) => {
+            console.log(p);
         });
     }
 
@@ -54,6 +73,23 @@ class WebsocketService {
      */
     static get RELATIVE_PATH() {
         return "/ws";
+    }
+
+    /**
+     * Check the user given credentials at each messages
+     * @param {object} query
+     */
+    async checkUserAccess(query) {
+        console.log('checkUserAccess');
+
+        if (!query) throw new AuthError(AUTH.ACCESS_DENIED + " a querry is requiered");
+        if (!query.playerId) throw new AuthError(AUTH.ACCESS_DENIED + " a playerId is requiered");
+        if (!query.quizId) throw new AuthError(AUTH.ACCESS_DENIED + " a querryId is requiered");
+
+        console.log('OK');
+        // TODO Check Credentials with DB
+
+        return { player: true, lobby: true }
     }
 }
 
