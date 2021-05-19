cf37ffd46d5436aca915da57071343865e4d5517
NGUYEN, Sophie <chaussettefurtive@outlook.fr>
2021-05-19 22:43:06 +0200

    Add tests for questions entrypoint

--- test/test-utils/server.js
+++ test/test-utils/server.js
@@ -6,7 +6,8 @@ const SERVER_URL = "http://localhost:" + serverConfig.port;
 const VERSION_ROUTE = "/version";
 
 // Quiz
-const QUIZ_ANSWER_ROUTE = (id) => "/quiz/" + id + "/answer";
+const QUIZ_ANSWER_ROUTE = (id) => `/quiz/${id}/answer`;
+const QUIZ_QUESTIONS_ROUTE = (id) => `/quiz/${id}/questions`;
 const QUIZ_LIST_ROUTE = "/quiz/list";
 
-export { SERVER_URL, VERSION_ROUTE, QUIZ_LIST_ROUTE, QUIZ_ANSWER_ROUTE };
+export { SERVER_URL, VERSION_ROUTE, QUIZ_LIST_ROUTE, QUIZ_QUESTIONS_ROUTE, QUIZ_ANSWER_ROUTE };
